7ce96b5e7fc545b47d471caad5e529ab7ed2dd8d
Maurício Freire <mauriciotcg3@gmail.com>
2023-10-18 17:56:15 -0300

    Categorias CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
-import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategorias/FormularioCategorias';
+import DeletarCategoria from './components/categorias/deletarCategorias/DeletarCategorias';
 
 
 function App() {
@@ -23,7 +25,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria/>} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria/>} />
+
             </Routes>
           </div>
           <Footer />
